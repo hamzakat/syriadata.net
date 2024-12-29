b8a0e5c0af8a8d506ccac6d8d06422ef1b27cc24
Hamza <hamzakattan@outlook.com>
2024-12-29 17:11:04 +0300

    Implement the landing page

---
+++ src/i18n/translations.ts
@@ -0,0 +1,16 @@
+export const translations = {
+    en: {
+      description: "Your first source for Syria-related data and research resources",
+      newBadge: "New",
+      exploreGuide: "Explore Syria Data Guide",
+      followUs: "Follow us on",
+      congrats: "Congrats to Syrians!"
+    },
+    ar: {
+      description: "مصدرك الأول لبيانات وموارد البحث المتعلقة بسوريا",
+      newBadge: "جديد",
+      exploreGuide: "استكشف دليل بيانات سوريا",
+      followUs: "تابعنا على",
+      congrats: "مبارك التحرير!"
+    }
+}
